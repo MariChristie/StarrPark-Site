9f3a4c539d49498ad2700519ad11033d9b04f4e9
= <cavaleiromiau@gmail.com>
2025-05-31 18:47:06 -0300

    mp4 fix

--- js/video.js
+++ js/video.js
@@ -1,9 +1,24 @@
 const containerPlayer = document.querySelector(".container-player");
 const playerVideo = containerPlayer.querySelector("video");
+const progressBar = containerPlayer.querySelector(".progress-bar");
 const playPauseBtn = containerPlayer.querySelector(".play-pause i");
+
+playerVideo.addEventListener("timeupdate", (e) => {
+    let { currentTime, duration } = e.target;
+    let percent = (currentTime / duration) * 100;
+    progressBar.style.width = `${percent}%`;
+
+});
 
 playPauseBtn.addEventListener("click", () => {
    playerVideo.paused ? playerVideo.play() : playerVideo.pause();
 });
 
+playerVideo.addEventListener("play", () => {
+    playPauseBtn.classList.replace("fa-play", "fa-pause");
+});
+
+playerVideo.addEventListener("pause", () => {
+    playPauseBtn.classList.replace("fa-pause", "fa-play");
+});
 
