9a4ff8ac3821acf9d9f34b1f4bc0ac2d6a0df688
= <cavaleiromiau@gmail.com>
2025-06-09 21:07:56 -0300

    Mod

--- js/home.js
+++ js/home.js
@@ -74,3 +74,12 @@ document.addEventListener("mouseup", () => {
     dragging = false;
     tabsList.classList.remove("dragging");
 });
+
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
